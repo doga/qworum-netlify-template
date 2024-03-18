565ea82dff55e1186230e657aa1de770d1baebb6
Doğa Armangil <doga.armangil@alumni.epfl.ch>
2024-03-18 17:58:42 +0100

    Use new JS lib version (bypasses NPM and has preliminary support for Safari)

--- src/assets/js/home.js
+++ src/assets/js/home.js
@@ -1,7 +1,7 @@
 
 // The Qworum library.
 // Documentation: https://qworum.net/docs/qworum-for-web-pages/latest/
-import { Qworum } from "https://esm.sh/@qworum/qworum-for-web-pages@1.0.11";
+import { Qworum } from "https://esm.sh/gh/doga/qworum-for-web-pages@1.2.0/mod.mjs";
 
 const
   // Qworum Data value types
